b4f2c5a943b59b01ba3fecc2e6c7cff0bf8e3bf5
MSteketee <70577986+MSteketee@users.noreply.github.com>
2021-04-06 20:20:22 +0200

    Add files via upload
    Alleen values samenvoegen naar means per dag gaat nog niet

--- Main_code.py
+++ Main_code.py
@@ -5,7 +5,12 @@ import pandas as pd
 
 # Lees data in
 data = pd.read_csv("data/dataset_mood_smartphone.csv", index_col = 0)
+# Verander time column naar dates
+data["time"] = pd.to_datetime(data["time"])
+data["time"] = data["time"].dt.date
+# Pivot naar elke rij 1 datum met alle variabelen
 data = data.pivot_table(values = ["value"], columns = "variable", index = ["time","id"])
-print(data.index.shape)
-#data.pivot(index = "time")
-#print(data)
+# Aggrereer naar dag niveau door variabelen te averagen per dag
+# Deze stap werkt nog niet, hieronder is een probeersel
+#data = data.groupby([data.index.levels[0].values, data.index.levels[1].values], as_index = False).mean()
+print(data.index.levels[0].values)
