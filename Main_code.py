95f504a2f60a40a3a5a35f061aa1ee27b49bfde9
MSteketee <70577986+MSteketee@users.noreply.github.com>
2021-04-06 20:29:30 +0200

    Add files via upload
    Gelukt, pivot.table had al als default mean aggregation

--- Main_code.py
+++ Main_code.py
@@ -9,8 +9,4 @@ data = pd.read_csv("data/dataset_mood_smartphone.csv", index_col = 0)
 data["time"] = pd.to_datetime(data["time"])
 data["time"] = data["time"].dt.date
 # Pivot naar elke rij 1 datum met alle variabelen
-data = data.pivot_table(values = ["value"], columns = "variable", index = ["time","id"])
-# Aggrereer naar dag niveau door variabelen te averagen per dag
-# Deze stap werkt nog niet, hieronder is een probeersel
-#data = data.groupby([data.index.levels[0].values, data.index.levels[1].values], as_index = False).mean()
-print(data.index.levels[0].values)
+data = data.pivot_table(values = ["value"], columns = "variable", index = ["time","id"]) # Default aggregation is al mean
