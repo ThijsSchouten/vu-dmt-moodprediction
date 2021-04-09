7198219d03d9e4c7b88c294950ca68d8dbd41238
Siem Teusink <siemteusink@hotmail.com>
2021-04-09 09:01:22 +0200

    Added normalisation, changed to function based script.

--- Main_code.py
+++ Main_code.py
@@ -3,10 +3,40 @@
 # Import packages
 import pandas as pd
 
-# Lees data in
-data = pd.read_csv("data/dataset_mood_smartphone.csv", index_col = 0)
-# Verander time column naar dates
-data["time"] = pd.to_datetime(data["time"])
-data["time"] = data["time"].dt.date
-# Pivot naar elke rij 1 datum met alle variabelen
-data = data.pivot_table(values = ["value"], columns = "variable", index = ["time","id"]) # Default aggregation is al mean
+def load_data(fname="data/dataset_mood_smartphone.csv")
+    # Lees data in
+    data = pd.read_csv("data/dataset_mood_smartphone.csv", index_col=0)
+
+    # Verander time column naar dates
+    data["time"] = pd.to_datetime(data["time"])
+    data["time"] = data["time"].dt.date
+
+    return data
+
+def pivot_average_data(data):
+    """
+    Averages all values per day and pivots
+    pandas table to use day and user id as index.
+    """
+    # Default aggregation is taking the mean.
+    new_dataset = data = data.pivot_table(
+        values=["value"], columns="variable", index=["time", "id"]
+    )
+
+    return new_dataset
+
+
+def normalize_data(data):
+    """
+    Normalizes data using min-max scaling
+    """
+    new_dataset = data.copy()
+    # All values except the 'call' and 'sms' column
+    # need to be normalised.
+    rel_columns = data.columns.drop([("value", "sms"), ("value", "call")])
+    for column in rel_columns:
+        new_dataset[column] = (data[column] - data[column].min()) / (
+            data[column].max() - data[column].min()
+        )
+
+    return new_dataset
