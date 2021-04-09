304f17326ebb09a68b1c7d54e6c0f5e40bf6519e
Siem Teusink <siemteusink@hotmail.com>
2021-04-09 13:24:44 +0200

    Added imputing of data and removing data

--- data.py
+++ data.py
@@ -3,6 +3,7 @@
 # Import packages
 import pandas as pd
 from math import isnan
+import numpy as np
 
 
 def load_data(fname="data/dataset_mood_smartphone.csv"):
@@ -23,7 +24,7 @@ def pivot_average_data(data):
     """
     # Default aggregation is taking the mean.
     new_dataset = data.pivot_table(
-        values=["value"], columns="variable", index=["time", "id"]
+        values=["value"], columns="variable", index=["id", "time"]
     )
 
     return new_dataset
@@ -45,20 +46,31 @@ def normalize_data(data):
     return new_dataset
 
 
+def remove_moodless_days(data):
+    data.dropna(subset=[("value", "mood")], inplace=True)
+
+
 def impute_missing_values(data, ids):
     """
     Imputes missing values with the mean over
     that variable for that user.
     """
-    mean_df = pd.DataFrame(index=ids, columns=data.value.columns)
+    # Missing values should be calculated for all categories but mood.
+    rel_columns = data.columns.drop([("value", "mood")])
+
+    # Loop over all users and all categories and replace NaNs with the mean
+    # for that user and that category.
     for id in ids:
-        for column in data:
-            mean = data.xs(id, level="id")[column].mean()
-            data.xs(id, level="id")[column].fillna(value=mean, inplace=True)
+        for column in rel_columns:
+            mean = data.loc[id][column].mean()
+            mean = 0 if isnan(mean) else mean
+            data.loc[id][column].fillna(value=mean, inplace=True)
 
 
 raw_data = load_data()
 ids = list(set(raw_data["id"]))
 ids.sort()
 data = pivot_average_data(raw_data)
+remove_moodless_days(data)
+impute_missing_values(data, ids)
 norm_data = normalize_data(data)
