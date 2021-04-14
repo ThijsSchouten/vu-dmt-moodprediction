0c2d634508bd15ffc302ddede70be9d19c382b3d
MSteketee <70577986+MSteketee@users.noreply.github.com>
2021-04-14 12:43:53 +0200

    Add files via upload

--- LSTM.py
+++ LSTM.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy
 import statistics
 from data import *
+from svm import *
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.models import Sequential
@@ -16,7 +17,9 @@ from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasRegressor
 
 
-def create_dataset(dataset, look_back=3):
+LOOK_BACK = 8 # Constant for how many days there should be looked back
+
+def create_dataset(dataset, look_back= LOOK_BACK):
     dataX, dataY = [], []
     for i in range(len(dataset) - look_back - 1):
         a = dataset.iloc[i:(i + look_back), 0]
@@ -25,9 +28,8 @@ def create_dataset(dataset, look_back=3):
     return numpy.array(dataX), numpy.array(dataY)
 
 
-def create_traintest(mood, look_back=3):
-    train_size = int(len(mood) * 0.67)
-    test_size = len(mood) - train_size
+def create_traintest(mood, look_back= LOOK_BACK):
+    train_size = int(len(mood) * 0.75)
     # mood is nu geordend per patient, dus de test set is nu alleen laatste paar patienten = niet goed
     train, test = mood.iloc[0:train_size, :], mood.iloc[train_size:len(mood), :]
     # reshape into X=t and Y=t+1
@@ -39,13 +41,12 @@ def create_traintest(mood, look_back=3):
     return trainX, trainY, testX, testY
 
 
-def create_LSTM(trainX, trainY, look_back, epochs=10, batch_size=1):
+def create_LSTM(look_back = LOOK_BACK):
     # create and fit the LSTM network
     model = Sequential()
     model.add(LSTM(4, input_shape=(1, look_back)))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(trainX, trainY, epochs=epochs, batch_size=batch_size, verbose=2)
     return model
 
 
@@ -56,13 +57,13 @@ def inverse_normalization2(trainPredict, trainY, testPredict, testY, scaler_fp='
 	"""
     scaler = load(open(scaler_fp, 'rb'))
     trainPredict = scaler.inverse_transform(trainPredict)
-    trainY = scaler.inverse_transform([trainY])
+    trainY = scaler.inverse_transform(trainY)
     testPredict = scaler.inverse_transform(testPredict)
-    testY = scaler.inverse_transform([testY])
+    testY = scaler.inverse_transform(testY)
     return trainPredict, trainY, testPredict, testY
 
 
-def shift_predictions_for_plots(mood, trainPredict, look_back, testPredict):
+def shift_predictions_for_plots(mood, trainPredict, testPredict, look_back = LOOK_BACK):
     # shift train predictions for plotting
     trainPredictPlot = numpy.empty_like(mood)
     trainPredictPlot[:, :] = numpy.nan
@@ -82,53 +83,44 @@ def plot_baseline_vs_predictions(mood, trainPredictPlot, testPredictPlot, scaler
     plt.plot(testPredictPlot)
     plt.show()
 
-
-def create_traintest(mood, look_back=3):
-    train_size = int(len(mood) * 1)
-    # mood is nu geordend per patient, dus de test set is nu alleen laatste paar patienten = niet goed
-    train = mood.iloc[0:train_size, :]
-    # reshape into X=t and Y=t+1
-    trainX, trainY = create_dataset(train, look_back)
-    # reshape input to be [samples, time steps, features]
-    trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-    return trainX, trainY
-
-
 data = preprocess_raw_data()
 ids = data.index.levels[0].values
 mood = data.loc[:, (slice(None), 'mood')]  # mood waardes van alle patienten
-look_back = 1  # aantal dagen dat je terugkijkt
 tscv = TimeSeriesSplit()
 train_results = dict()
 test_results = dict()
 for id in ids:
-    trainX, trainY = create_traintest(mood.loc[id], look_back)
-    batch_size = [1, 2, 3]
-    epochs = [5, 10, 15]
-    model = KerasRegressor(build_fn = create_LSTM(trainX, trainY, look_back), verbose =1)
+    trainX, trainY, testX, testY = create_traintest(mood.loc[id])
+    batch_size = [2, 4, 8]
+    epochs = [10, 20, 40]
     param_grid = dict(epochs=epochs, batch_size=batch_size)
+    model = KerasRegressor(build_fn = create_LSTM, verbose =0)
     grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=tscv, verbose=0)
     resultgridsearch = grid.fit(trainX, trainY)
-    best_params = clf.best_params_
-    model = KerasRegressor(create_LSTM(trainX, trainY, look_back),best_params)
-# # Make predictions
-# trainPredict = model.predict(trainX)
-# testPredict = model.predict(testX)
-# # Invert predictions
-# trainPredict, trainY, testPredict, testY = inverse_normalization2(trainPredict,trainY,testPredict,testY)
-# # calculate root mean squared error
-# trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
-# #print('Train Score: %.2f RMSE' % (trainScore))
-# testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
-# #print('Test Score: %.2f RMSE' % (testScore))
-# train_results[id] = [trainScore, train_index]
-# test_results[id] = [testScore, test_index]
-# 	# trainPredictPlot, testPredictPlot = shift_predictions_for_plots(mood, trainPredict, look_back, testPredict)
-# 	# plot_baseline_vs_predictions(mood,trainPredictPlot,testPredictPlot)
+    best_params = resultgridsearch.best_params_
+    print(best_params)
+    # Make predictions
+    trainPredict = resultgridsearch.predict(trainX)
+    testPredict = resultgridsearch.predict(testX)
+    # Reshape data
+    trainY = trainY.reshape(-1, 1)
+    testY = testY.reshape(-1, 1)
+    trainPredict = trainPredict.reshape(-1,1)
+    testPredict = testPredict.reshape(-1,1)
+    # Invert predictions
+    trainPredict, trainY, testPredict, testY = inverse_normalization2(trainPredict, trainY, testPredict, testY)
+    # calculate root mean squared error
+    trainScore = math.sqrt(mean_squared_error(trainY, trainPredict))
+    #print('Train Score: %.2f RMSE' % (trainScore))
+    testScore = math.sqrt(mean_squared_error(testY, testPredict))
+    #print('Test Score: %.2f RMSE' % (testScore))
+    train_results[id] = trainScore
+    test_results[id] = testScore
+    # # trainPredictPlot, testPredictPlot = shift_predictions_for_plots(mood, trainPredict, testPredict)
+    # # plot_baseline_vs_predictions(mood,trainPredictPlot,testPredictPlot)
 
-#
-# print(statistics.mean(train_results.values()), statistics.mean(test_results.values()))
-# print(statistics.variance(train_results.values()), statistics.variance(test_results.values()))
+print('Train Score: %.2f RMSE' % (statistics.mean(train_results.values())), 'Test Score: %.2f RMSE' % (statistics.mean(test_results.values())))
+print('Train Variance: %.2f RMSE' % (statistics.variance(train_results.values())), 'Test Variance: %.2f RMSE' % (statistics.variance(test_results.values())))
 
 
 ########################################################################################################################
