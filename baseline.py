c056714fd7eecd0069c98bed83e258c973c27753
Siem Teusink <siemteusink@hotmail.com>
2021-04-14 16:13:22 +0200

    Minor changes baseline

--- baseline.py
+++ baseline.py
@@ -1,5 +1,6 @@
 from data import *
 import pandas as pd
+import numpy as np
 
 
 def baseline_predictor(instance):
@@ -13,13 +14,18 @@ def evaluate_model(model, instances, labels):
 
 
 def run_baseline_model(no_days=5):
-    instances, labels = get_baseline_data(no_days=no_days)
+    instances, labels = get_baseline_data(no_days=no_days)`
+    indices = np.arange(len(instances))
     split = int(0.8 * len(instances))
-    test_data, test_labels = instances[split:], labels[split:]
+    test_data, test_labels = (
+        instances[indices][split:],
+        labels[indices][split:],
+    )
     model = baseline_predictor
     return evaluate_model(model, np.array(test_data), np.array(test_labels))
 
 
 if __name__ == "__main__":
+    np.random.seed(42)
     mse = run_baseline_model()
     print(mse)
